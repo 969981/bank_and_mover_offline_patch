c588d5508af34f481549e0ca3f57e929e1e15de7
969981 <58679844+969981@users.noreply.github.com>
2026-09-23 12:46:35 +0800

    feat(bank): add staged smart recovery marker

--- bank/src/official_recovery_marker.h
+++ bank/src/official_recovery_marker.h
@@ -7,6 +7,16 @@
 #define OFFICIAL_RECOVERY_MARKER_FLAG_BULK_APPLIED 0x0001u
 #define OFFICIAL_RECOVERY_MARKER_FLAG_BULK_PENDING 0x0002u
 
+typedef enum {
+    OFFICIAL_RECOVERY_STAGE_NONE=0,
+    OFFICIAL_RECOVERY_STAGE_BULK_APPLIED=1,
+    OFFICIAL_RECOVERY_STAGE_TX_BOUND=2,
+    OFFICIAL_RECOVERY_STAGE_GAME_SAVE_STARTED=3,
+    OFFICIAL_RECOVERY_STAGE_GAME_SAVE_OK=4,
+    OFFICIAL_RECOVERY_STAGE_REMOTE_COMPLETE_STARTED=5,
+    OFFICIAL_RECOVERY_STAGE_DONE=6
+} OfficialRecoveryStage;
+
 int OfficialRecoveryMarker_Encode(unsigned char out[OFFICIAL_RECOVERY_MARKER_SIZE],
     const OfficialRecoveryRecord *server,unsigned profile,unsigned flags);
 int OfficialRecoveryMarker_EncodePending(
@@ -21,6 +31,12 @@ int OfficialRecoveryMarker_Decode(const unsigned char marker[OFFICIAL_RECOVERY_M
 int OfficialRecoveryMarker_MatchesServer(
     const unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE],
     const OfficialRecoveryRecord *server,unsigned profile);
+int OfficialRecoveryMarker_GetStage(
+    const unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE],
+    OfficialRecoveryStage *stageOut);
+int OfficialRecoveryMarker_AdvanceStage(
+    unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE],
+    OfficialRecoveryStage stage);
 void OfficialRecoveryMarker_Rechecksum(
     unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE]);
 
