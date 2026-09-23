d71d1f98ef5732e800f28244579b38957999f56f
969981 <58679844+969981@users.noreply.github.com>
2026-09-23 11:26:35 +0800

    feat(bank): define bulk recovery transaction marker

---
+++ bank/src/official_recovery_marker.h
@@ -0,0 +1,19 @@
+#ifndef BANK_OFFICIAL_RECOVERY_MARKER_H
+#define BANK_OFFICIAL_RECOVERY_MARKER_H
+
+#include "official_recovery_probe.h"
+
+#define OFFICIAL_RECOVERY_MARKER_SIZE 48u
+#define OFFICIAL_RECOVERY_MARKER_FLAG_BULK_APPLIED 0x0001u
+
+int OfficialRecoveryMarker_Encode(unsigned char out[OFFICIAL_RECOVERY_MARKER_SIZE],
+    const OfficialRecoveryRecord *server,unsigned profile,unsigned flags);
+int OfficialRecoveryMarker_Decode(const unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE],
+    OfficialRecoveryRecord *serverOut,unsigned *profileOut,unsigned *flagsOut);
+int OfficialRecoveryMarker_MatchesServer(
+    const unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE],
+    const OfficialRecoveryRecord *server,unsigned profile);
+void OfficialRecoveryMarker_Rechecksum(
+    unsigned char marker[OFFICIAL_RECOVERY_MARKER_SIZE]);
+
+#endif
