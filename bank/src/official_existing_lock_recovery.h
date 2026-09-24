4edf4931064488cd4c406a58ea885216b9233eb9
969981 <58679844+969981@users.noreply.github.com>
2026-09-24 09:27:16 +0800

    feat(bank): define existing-lock recovery policy

---
+++ bank/src/official_existing_lock_recovery.h
@@ -0,0 +1,32 @@
+#ifndef BANK_OFFICIAL_EXISTING_LOCK_RECOVERY_H
+#define BANK_OFFICIAL_EXISTING_LOCK_RECOVERY_H
+
+typedef struct {
+    unsigned long long dataId;
+    unsigned curVersion;
+    unsigned updateVersion;
+    unsigned size;
+    unsigned reserved;
+    unsigned long long transactionPassword;
+} OfficialExistingLockServerTx;
+
+typedef struct {
+    unsigned long long dataId;
+    unsigned long long transactionPassword;
+    unsigned curVersion;
+    unsigned updateVersion;
+    unsigned size;
+    unsigned char status;
+} OfficialExistingLockGameRecord;
+
+typedef enum {
+    OFFICIAL_EXISTING_LOCK_KEEP_STOCK_MISMATCH=0,
+    OFFICIAL_EXISTING_LOCK_ROUTE_STATE17_ROLLBACK=1
+} OfficialExistingLockDecision;
+
+OfficialExistingLockDecision OfficialExistingLock_PrepareGameRollback(
+    int stockMismatch,
+    const OfficialExistingLockServerTx *server,
+    OfficialExistingLockGameRecord *gameOut);
+
+#endif
